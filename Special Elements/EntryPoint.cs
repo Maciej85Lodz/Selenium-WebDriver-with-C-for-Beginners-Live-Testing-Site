b599097af8076801390792bc024272e65210689f
maciej.wolejszo <maciej.wolejszo@gmail.com>
2020-03-24 22:53:34 +0100

    Special Characters; Input text box

---
+++ Special Elements/EntryPoint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Remoting.Channels;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class EntryPoint
+{
+    static IWebDriver driver = new ChromeDriver();
+    static IWebElement textBox;
+
+    static void Main()
+    {
+        string url = "http://testing.todvachev.com/special-elements/text-input-field/";
+        
+        driver.Navigate().GoToUrl(url);
+
+        textBox = driver.FindElement(By.Name("username"));
+
+        textBox.SendKeys("Test Text");
+
+        Thread.Sleep(3000);
+
+        Console.WriteLine(textBox.GetAttribute("value"));
+        Console.WriteLine(textBox.GetAttribute("maxlenght"));
+
+        Thread.Sleep(3000);
+
+        driver.Quit();
+    }
+
+}
