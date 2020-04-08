e8e89bb7d4232311db4bca7ef8e964d80508466f
maciej.wolejszo <maciej.wolejszo@gmail.com>
2020-04-08 15:31:20 +0200

    AutoTestFramework - part 2

---
+++ AutoTestFramework/UIElements/TestCasesPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace AutoTestFramework.UIElements
+{
+    public class TestCasesPage
+    {
+        public TestCasesPage()
+        {
+            PageFactory.InitElements(Driver.driver, this);
+        }
+
+        [FindsBy(How = How.CssSelector, Using = "#main-content > article.mh-loop-item.clearfix.post-74.post.type-post.status-publish.format-standard.has-post-thumbnail.hentry.category-test-cases > div.mh-loop-content.clearfix > header > h3 > a")]
+        public IWebElement UsernameField { get; set; }
+    }
+}
