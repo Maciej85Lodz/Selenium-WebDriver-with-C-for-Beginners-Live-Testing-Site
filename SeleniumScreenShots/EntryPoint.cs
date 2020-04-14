5715231536c030f06c73760ca0edec38e2c617b3
maciej.wolejszo <maciej.wolejszo@gmail.com>
2020-04-14 11:23:48 +0200

    Selenium with screenshots

---
+++ SeleniumScreenShots/EntryPoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumScreenShots
+{
+    class EntryPoint
+    {
+        static void Main()
+        {
+            IWebDriver chrome = new ChromeDriver();
+
+            string screenshotsDirectory = Directory.GetCurrentDirectory() + @"\screenshots";
+
+            chrome.Navigate().GoToUrl("http://google.com");
+
+            Screenshot googleScreenshot = ((ITakesScreenshot)chrome).GetScreenshot();
+
+            if (!Directory.Exists(screenshotsDirectory))
+            {
+                Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"\screenshots");
+            }
+            googleScreenshot.SaveAsFile(Directory.GetCurrentDirectory() + @"\screenshots\googlescreenshot.png",ScreenshotImageFormat.Png);
+
+
+            chrome.Quit();
+        }
+    }
+}
