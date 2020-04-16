e0f77f0eb47b075b3c47fda7023c1f685b2c94b5
maciej.wolejszo <maciej.wolejszo@gmail.com>
2020-04-16 23:57:43 +0200

    Execute JavaScript with C# and Modify the Stylings of an Element

--- ElementLocations/EntryPoint.cs
+++ ElementLocations/EntryPoint.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
@@ -14,14 +16,47 @@ namespace ElementLocations
             driver.Navigate().GoToUrl(url);
 
             IWebElement image = driver.FindElement(By.CssSelector("#page-17 > div > p:nth-child(1) > a > img"));
-            driver.Manage().Window.Maximize();
+            //driver.Manage().Window.Maximize();
 
-            Console.WriteLine(image.Location.X);
-            Console.WriteLine(image.Location.Y);
-            Console.WriteLine(image.Size.Width);
-            Console.WriteLine(image.Size.Height);
+            //Console.WriteLine(image.Location.X);
+            //Console.WriteLine(image.Location.Y);
+            //Console.WriteLine(image.Size.Width);
+            //Console.WriteLine(image.Size.Height);
+
+            IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+
+            string script = "arguments[0].style[\"display\"]=\"none\"";
+
+            jsExecutor.ExecuteScript(script, image);
+
+            IWebElement textColor = driver.FindElement(By.CssSelector("#page-17 > div "));
+
+            script = "arguments[0].style[\"color\"]=\"red\"";
+            jsExecutor.ExecuteScript(script, textColor);
+
+            string script2 = "arguments[0].style[\"border\"]=\"5px solid red\"";
+
+            jsExecutor.ExecuteScript(script2, textColor);
 
 
+            Thread.Sleep(2000);
+
+            IWebElement content = driver.FindElement(By.CssSelector("#page-17 > div "));
+
+            SetStyle(driver, content, "color","green");
+
+            Thread.Sleep(3000);
+
+            driver.Quit();
+
+        }
+
+        static void SetStyle(IWebDriver driver, IWebElement element, string style, string styleValue)
+        {
+            string script = String.Format("arguments[0].style[\"{0}\"] = \"{1}\"", style,styleValue);
+
+            IJavaScriptExecutor  jsExecutor = (IJavaScriptExecutor)driver;
+            jsExecutor.ExecuteScript(script, element);
         }
     }
 }
